f37e2a30281802d05c3d236d7b84ed3538e6bafb
hp77-creator <himanshu7pandey7@gmail.com>
2020-03-26 11:57:09 +0530

    Adding Files

---
+++ dashboard/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your motr__(self):
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=150)
+    position = models.CharField(max_length=150)
+    office = models.CharField(max_length=150)
+    age = models.PositiveIntegerField()
+    start_date = models.DateField()
+    salary = models.PositiveIntegerField()
+
+
+    def __str__(self):
+        return self.name
+
